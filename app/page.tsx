09164c19643e896551566c2ebd559269b64fcbcd
Jakub Matusiak <kontakt@jakub-matusiak.pl>
2024-05-14 23:30:57 +0200

    feat: update the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="container">
-      <h1>Homepage</h1>
+    <main className='flex flex-col flex-1 w-full px-4 md:px-16 py-4'>
+      <div className='mx-auto max-w-7xl w-full'>
+        <h1>Homepage</h1>
+      </div>
     </main>
   );
 }

4709e2f71207f285c2ca3e47e26c3e9a610e431a
Jakub Matusiak <kontakt@jakub-matusiak.pl>
2024-06-11 23:30:54 +0200

    feat: add daisyUI for Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,11 @@ const config: Config = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ['light'],
+  }
 };
 export default config;

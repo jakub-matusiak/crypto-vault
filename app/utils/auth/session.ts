fe8f1e3c5fd0cb6dd9e9c3acb461813024fbf61c
Jakub Matusiak <kontakt@jakub-matusiak.pl>
2024-07-11 20:54:47 +0200

    fix: change secret key variable for auth

--- app/utils/auth/session.ts
+++ app/utils/auth/session.ts
@@ -7,7 +7,7 @@ import { SignJWT, jwtVerify } from 'jose';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-const secretKey = process.env.SECRET;
+const secretKey = process.env.AUTH_SECRET;
 const key = new TextEncoder().encode(secretKey);
 
 export async function encrypt(payload: SessionPayload) {

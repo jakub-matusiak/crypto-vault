8befdbda46451fdcde22bfc87e37b8c5526507d0
Jakub Matusiak <kontakt@jakub-matusiak.pl>
2024-07-29 17:58:32 +0200

    feat: add button to handle add/remove crypto from watchlist

--- app/components/Watchlist/WatchlistButton.tsx
+++ app/components/Watchlist/WatchlistButton.tsx
@@ -2,20 +2,33 @@
 
 import Image from 'next/image';
 
+import { useState } from 'react';
+
 type WatchlistButtonType = {
   width: number,
   height: number,
 }
 
 export default function WatchlistButton({ width, height }: WatchlistButtonType) {
+  const [active, setActive] = useState<boolean>(false);
+  const [tooltip, setTooltip] = useState<boolean>(false);
+
   function handleClick() {
-    console.log('add to watchlist');
+    setActive(!active);
+    setTooltip(true);
+
+    setTimeout(() => setTooltip(false), 3000);
   }
 
   return (
-    <button onClick={handleClick}>
-      <Image src='/star-outline.svg' className={`min-w-[16px] min-h-[16px]`} width={width} height={height} alt='' />
-      {/* <Image src='/star-filled.svg' className={`min-w-[16px] min-h-[16px]`} width={width} height={height} alt='' /> */}
-    </button>
+    <div className={tooltip ? 'tooltip tooltip-right tooltip-open' : ''} data-tip={active ? 'Added to Watchlist!' : 'Removed from Watchlist!'}>
+      <button onClick={handleClick}>
+        {active ? (
+          <Image src='/star-filled.svg' className={`min-w-[16px] min-h-[16px]`} width={width} height={height} alt='' />
+        ) : (
+          <Image src='/star-outline.svg' className={`min-w-[16px] min-h-[16px]`} width={width} height={height} alt='' />
+        )}
+      </button>
+    </div>
   );
 }
